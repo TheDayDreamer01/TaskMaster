b5aff9304a51dfaf872ec6eca738253e85f37ee7
Lirae Que Data <liraedata59@gmail.com>
2023-05-12 23:06:52 +0800

    Fresh Start

--- lib/const/light_theme_const.dart
+++
@@ -1,5 +0,0 @@
-import "package:flutter/material.dart";
-
-ThemeData lightTheme = ThemeData(
-  useMaterial3: true 
-);
--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,19 @@
 import "package:flutter/material.dart";
-import "package:firebase_core/firebase_core.dart";
-import "package:provider/provider.dart";
 
 
-
-import "package:taskmaster/const/light_theme_const.dart";
-import "package:taskmaster/const/dark_theme_const.dart";
-
-
-import "package:taskmaster/screens/auth/auth.dart";
-import "package:taskmaster/screens/onboard/onboard.dart";
-
-
-import "package:taskmaster/providers/settings_provider.dart";
-
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-
-  runApp(
-    MultiProvider(
-      providers : [
-        ChangeNotifierProvider(
-          create : (context) => SettingsProvider()
-        ),
-        
-      ],
-      child : const TaskMasterApp()
-    )
-  );
+void main(){
+  runApp( const TaskMasterApp() );
 }
 
-
-class TaskMasterApp extends StatelessWidget {
-  const TaskMasterApp({ super.key });
+class TaskMasterApp extends StatelessWidget{
+  const TaskMasterApp({ Key ? key }) : super( key : key );
 
   @override
   Widget build(BuildContext context){
-    
-    final wSettings = context.watch<SettingsProvider>();
-    final isFirstInstall = wSettings.getBoolPreference("isFirstInstall");
-    final isDarkMode = wSettings.getBoolPreference("isDarkMode");
-    
     return MaterialApp(
-      title : "TaskMaster",
+      title : "Task Master",
       debugShowCheckedModeBanner: false,
-
-      theme : lightTheme,
-      darkTheme: darkTheme,
-      themeMode : (!isDarkMode) ?
-        ThemeMode.light :
-        ThemeMode.dark,
-
-      home : (isFirstInstall) ? 
-        OnboardView() :
-        const AuthView()
+      home : Scaffold()
     );
   }
 }
